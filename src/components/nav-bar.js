0155061c4a4d2fcebb2812589d3c4e8e1a99495e
Stav Davis <stav.davis@gmail.com>
2018-02-01 16:14:03 -0800

    added react-router Link components instead of regular links in nav bar

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -3,16 +3,17 @@
 //<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">
 import React from 'react';
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import {Link, Redirect} from 'react-router-dom';
 import './nav-bar.css';
 
 export default class MainNavBar extends React.Component {
   render() {
     let rightNavLinks = (
       <Nav>
-        <NavItem className="navbar-app-link" href="/about">About</NavItem>
-        <NavItem className="navbar-app-link" href="/dashboard">Dashboard</NavItem>
-        <NavItem className="navbar-app-link" href="/stats">Stats & news</NavItem>
-        <NavItem className="navbar-app-link" href="/register">Sign up</NavItem>
+        <NavItem><Link className="navbar-app-link" to="/about">About</Link></NavItem>
+        <NavItem><Link className="navbar-app-link" to="/dashboard">Dashboard</Link></NavItem>
+        <NavItem><Link className="navbar-app-link" to="/stats">Stats & News</Link></NavItem>
+        <NavItem><Link className="navbar-app-link" to="/register">Signup</Link></NavItem>
       </Nav>
     );
     return (
@@ -20,7 +21,7 @@ export default class MainNavBar extends React.Component {
         <Navbar.Header>
           <Navbar.Toggle />
           <Navbar.Brand>
-            <a className="app-name" href="/">Politismart</a>
+            <Link className="app-name" to="/">Politismart</Link>
           </Navbar.Brand>
         </Navbar.Header>
         <Navbar.Collapse>
