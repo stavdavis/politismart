cced55f4f2739c24909fe92d37b540f526f61b13
Stav Davis <stav.davis@gmail.com>
2018-02-05 14:02:02 -0800

    imported bootstraps css files locally, because the remote link from index.html was not stalbe and was screwing up the formatting of the navbar. See nav-bar-bootstrap.css and nav-bar-bootstrap-theme.css

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,9 +1,14 @@
 //Based on: https://github.com/hjmccain/maplyful
-//IMPORTANT: Don't forget to import bootstraps' basic css file into the main index.html
+//IMPORTANT:
+//Don't forget to import bootstraps' basic css file into the main index.html: 
 //<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">
+//Here we've imported the actual css files (generated at https://getbootstrap.com/docs/3.3/customize/) - b/c the remote link was not stable
+//see nav-bar-bootstrap.css and nav-bar-bootstrap.theme.css
 import React from 'react';
 import { Nav, Navbar, NavItem } from 'react-bootstrap';
 import {Link} from 'react-router-dom';
+import './nav-bar-bootstrap.css';
+import './nav-bar-bootstrap-theme.css';
 import './nav-bar.css';
 
 export default class MainNavBar extends React.Component {
